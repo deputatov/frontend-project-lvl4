fff3277ab8946fe4dfd2c441668c60cd4af8de7a
Deputatov Nikita <deputatovn@yandex.ru>
2020-05-19 12:21:10 +0300

    rename to init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
-import app from './app';
+import run from './init';
 
-app();
+run();
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
